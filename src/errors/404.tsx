0fab2bbee8e8e179f2100cfeacde88d7fcae1946
LordAkavone <vcoffice.praetoraka@gmail.com>
2025-08-02 09:36:34 +0800

    Update 404.tsx

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <section class="bg-red-100 text-slate-700 p-8 rounded-md">
-      <h2 class="text-2xl">404: Not Found</h2>
-      <p class="mt-4">It's gone 😞!</p>
+      <h2 class="text-2xl">404: Page Not Found</h2>
+      <p class="mt-4">Brain not found.</p>
     </section>
   );
 }
