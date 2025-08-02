7528163e122443299f5717a50b44680f3e742bad
LordAkavone <vcoffice.praetoraka@gmail.com>
2025-08-02 09:44:09 +0800

    Update 404.tsx

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,8 +1,9 @@
 export default function NotFound() {
   return (
     <section class="bg-red-100 text-slate-700 p-8 rounded-md">
-      <h2 class="text-2xl">404: Page Not Found</h2>
-      <p class="mt-4">Brain not found.</p>
+      <h2 class="text-2xl">404: Brain Not Found</h2>
+      <p class="mt-4">Do you have any brain cells? Try again later.</p>
+      <img src ="Gemini_Generated_Image_o0jh8po0jh8po0jh.png" alt = "Gemini_Generated_Image_o0jh8po0jh8po0jh.png" />
     </section>
   );
 }
