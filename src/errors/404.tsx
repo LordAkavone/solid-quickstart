a34a65aa73b7a2f657293bb19d798be036a9ccd1
LordAkavone <vcoffice.praetoraka@gmail.com>
2025-08-03 15:33:04 +0800

    Update 404.tsx

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -3,7 +3,6 @@ export default function NotFound() {
     <section class="bg-red-100 text-slate-700 p-8 rounded-md">
       <h2 class="text-2xl">The Epic Dou Sha Bread Saga</h2>
       <p class="mt-4">The Dou Sha Bread Saga. A simple moment of joy, a warm dou sha bread, was shattered by a shadow-feathered thief—a crow. We battled, a grip-to-beak showdown on the pavement. The bird's grip was strong, but mine was "dam hard sia." I won the fight. But the victory was hollow. The brief, frantic struggle left the bread tainted. With a heavy heart, I had to throw it away. My anger wasn't for the crow. That bird was just doing what it was taught. The real blame lies with the humans who feed them, turning wild animals into aggressive opportunists. The dou sha bread was a casualty of a world out of balance, a simple meal lost to a cycle of human kindness and unintended consequences. What do you think is the best way to handle situations like this?.</p>
-      <img src="Gemini_Generated_Image_o0jh8po0jh8po0jh.png" alt="Gemini_Generated_Image_o0jh8po0jh8po0jh.png" />
     </section>
   );
 }
