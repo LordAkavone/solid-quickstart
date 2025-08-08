68f86f7e713f383f7531e84d3ae2846da33c3780
LordAkavone <vcoffice.praetoraka@gmail.com>
2025-08-08 14:06:16 +0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,8 @@
-import { createEffect, Suspense } from "solid-js";
+import { Suspense } from "solid-js";
 import AboutData from "./about.data";
 
 export default function About() {
   const name = AboutData();
-
-  createEffect(() => {
-    console.log(name());
-  });
 
   return (
     <section class="bg-indigo-100 text-slate-700 p-8 rounded-md">
