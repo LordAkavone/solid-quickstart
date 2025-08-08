5909fcde0c07440ad8441cff87a3d07a16dd5cbd
LordAkavone <vcoffice.praetoraka@gmail.com>
2025-08-08 14:02:58 +0800

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,29 +2,19 @@ import { createEffect, Suspense } from "solid-js";
 import AboutData from "./about.data";
 
 export default function About() {
-  const name = AboutData();
+  const name = AboutData();
 
-  createEffect(() => {
-    console.log(name());
-  });
+  createEffect(() => {
+    console.log(name());
+  });
 
-  return (
-    <section class="bg-indigo-100 text-slate-700 p-8 rounded-md">
-      <h2 class="text-2xl">About</h2>
+  return (
+    <section class="bg-indigo-100 text-slate-700 p-8 rounded-md">
+      <h2 class="text-2xl">About</h2>
 
-      <p class="mt-4">
-        My name is Akavone, and I code Roblox games as well as HTML. Contact vcoffice.praetoraka@gmail.com for business enquiries.
-        <Suspense fallback={<span>...</span>}>
-          <span>&nbsp;{name()}!</span>
-        </Suspense>
-      </p>
-      <p>
-        Visit{" "}
-        <a href="https://akaone.netlify.app/about" target="_blank" class="underline">
-      
-        </a>
-        {" "}for more information.
-      </p>
-    </section>
-  );
+      <p class="mt-4">
+        I am Akavone, a Praetor in BlueLitStudio's GAR. I am responsible for investigation on different individuals, including the Chancellor. Together with two other Praetors, we manage Blue's Roblox Star Wars game.
+      </p>
+    </section>
+  );
 }
